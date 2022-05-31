18684d1ae7dae12bdd373fdca638ada881824702
Olemish <olesia.hlushenkova@gmail.com>
2022-05-31 17:58:06 +0200

    change task 6 str 13

--- js/task-06.js
+++ js/task-06.js
@@ -10,7 +10,7 @@ const focusInput = document.querySelector("#validation-input")
 focusInput.addEventListener("blur", e => inputHandle(e))
 function inputHandle(e) {
     console.log(focusInput.getAttribute("data-length"))
-    if (e.target.value.length===Number(focusInput.getAttribute("data-length"))) {
+    if (e.target.value.length==focusInput.getAttribute("data-length")) {
     focusInput.classList.add("valid")
     } else {
      focusInput.classList.add("invalid")   
